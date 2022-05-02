63430107c51814f2bc9e7ef113b346e8a8f9327b
서성조 <67176549+seongjo-seo@users.noreply.github.com>
2022-05-03 01:58:55 +0900

    [Bronze III] Title: 별 찍기 - 2, Time: 288 ms, Memory: 17272 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[]args) throws IOException {	
+    	try {
+			BufferedReader s_rd = new BufferedReader(new InputStreamReader (System.in));
+			
+			String text[] = s_rd.readLine().split(" ");
+			int a = Integer.parseInt(text[0]);
+			
+			for(int i=1; i<=a;i++) {
+				for(int j=a;j>i;j--) {
+					System.out.print(" ");
+				}
+				for(int j=0;j<i;j++) {
+					System.out.print("*");					
+				}
+				System.out.println();
+			}
+		}
+		catch(IOException e) {
+		}
+	}
+}
