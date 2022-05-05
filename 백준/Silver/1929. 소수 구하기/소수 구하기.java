7a43d7710fcd745b50e2242d2c05142b9c296cfa
서성조 <67176549+seongjo-seo@users.noreply.github.com>
2022-05-05 16:42:24 +0900

    [Silver III] Title: 소수 구하기, Time: 1056 ms, Memory: 37276 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,25 +5,23 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[]args) throws IOException {
     	try {
-    		// 최댓값
-    		final int MAX = 1000000;
 			BufferedReader s_rd = new BufferedReader(new InputStreamReader (System.in));
 			String text[] = s_rd.readLine().split(" ");
 			// M = 시작 N = 끝
 			int M = Integer.parseInt(text[0]);
 			int N = Integer.parseInt(text[1]);
-			// 소수 값 확인을 위한 배열 선언 및 최대 크기 지정
-			boolean is_Prime[]= new boolean[MAX];
-			// 모든 배열 Arrays 클래스를 사용해서 false로 변경
-			Arrays.fill(is_Prime, false);
-			// 0과 1은 소수가 아니다. = true 값으로 변경
-			is_Prime[0] = is_Prime[1]=true;
+			boolean is_Prime[] = new boolean[N+1];
 			
-			for(int i=2;i<=Math.sqrt(N);i++){
+			for(int i=2;i<=N;i++){
+				if(is_Prime[i])
+					continue;
+				// boolean은 초기화시 기본 false을 갖는다.
+					
 				for(int j=2;i*j<=N;j++) {
 					is_Prime[i*j]=true;
 				}
 			}
+			is_Prime[0] = is_Prime[1] =true;
 			for(int i=M;i<=N;i++){
 				if(is_Prime[i]==false)
 					System.out.println(i);
@@ -34,3 +31,4 @@ public class Main {
 		}
 	}
 }
+// https://www.acmicpc.net/source/12599998 참조
