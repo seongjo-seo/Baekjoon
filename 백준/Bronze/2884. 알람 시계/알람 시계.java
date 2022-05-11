a8acb168724359f6a36f55d04a9ad1e360b24070
서성조 <67176549+seongjo-seo@users.noreply.github.com>
2022-05-12 01:00:03 +0900

    [Bronze III] Title: 알람 시계, Time: 80 ms, Memory: 11572 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -10,6 +10,15 @@ public class Main {
 			int H = Integer.parseInt(text[0]);
 			int M = Integer.parseInt(text[1]);
 			
+            // H이 24을 넘지 않는다.
+			// M이 60을 넘지 않는다.
+			if(H>23) {
+				H=0;
+			}
+			if(M>60) {
+				M=0;
+			}
+            
 			if(M<45) {
 				H--;
 				M =(60-(45-M));
@@ -20,22 +29,10 @@ public class Main {
 			}else if (M>=45) {
 				M-=45;
 			}
-			
-			// H이 24을 넘지 않는다.
-			// M이 60을 넘지 않는다.
-			if(H>23) {
-				H=0;
-			}
-			if(M>60) {
-				M=0;
-			}
-			
 			System.out.println(H + " "+ M);
-			
 		}
 		catch(IOException e) {
 			System.out.println("error");
 		}
-		
 	}
 }
