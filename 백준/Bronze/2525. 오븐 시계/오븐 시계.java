ef74a42354421d7d0599e5e737f6078c507232e5
서성조 <67176549+seongjo-seo@users.noreply.github.com>
2022-05-12 15:14:51 +0900

    [Bronze IV] Title: 오븐 시계, Time: 76 ms, Memory: 11576 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+// 2525번
+public class Main {
+	public static void main(String[] args) throws IOException {
+		try {
+			BufferedReader rd = new BufferedReader(new InputStreamReader (System.in));
+			String text[] = rd.readLine().split(" ");
+			
+			int A = Integer.parseInt(text[0]);
+			int B = Integer.parseInt(text[1]);
+			int cook = Integer.parseInt(rd.readLine());
+			
+			B+= cook;
+			while(B>59) {
+				A+=1;
+				B-=60;
+				B = Math.abs(B);
+				if(A>23) {
+					A=0;
+				}
+			}
+			System.out.println(A + " " + B);
+		}
+		catch(IOException e) {
+			System.out.println("error");
+		}
+	}
+}
