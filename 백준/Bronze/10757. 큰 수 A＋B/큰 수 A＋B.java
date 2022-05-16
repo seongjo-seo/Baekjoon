5855874655e2a914c5a5058cae5318a18e92c26f
서성조 <67176549+seongjo-seo@users.noreply.github.com>
2022-05-16 22:53:06 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 192 ms, Memory: 15080 KB -BaekjoonHub

--- 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.math.BigInteger;
-// 1712번
+// 10757번
 public class Main {
 	public static void main(String[] args)throws IOException {
 		
