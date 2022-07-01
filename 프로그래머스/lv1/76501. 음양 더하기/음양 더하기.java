4d81966b858aad966da67946f0ccd39fdf3dd814
서성조 <67176549+seongjo-seo@users.noreply.github.com>
2022-07-01 23:40:01 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int res=0;
+        
+        for(int i=0;i<absolutes.length;i++){
+            if(signs[i]==true){
+                res += absolutes[i];
+            }else{
+                res -= absolutes[i];
+            }
+        }
+        return res;
+    }
+}
