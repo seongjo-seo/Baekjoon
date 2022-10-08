3f5fd4233def93de40f34dbec96de4409f2d9a8d
서성조 <67176549+seongjo-seo@users.noreply.github.com>
2022-10-09 00:26:54 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
@@ -0,0 +1,13 @@
+while(True):
+    name, age, weight = input().split()
+
+    age = int(age)
+    weight = int(weight)
+    
+    if(name=='#' and age==0 and weight==0 ):
+        break;
+
+    if( age>17 or weight>=80 ):
+        print(f"{name} Senior")
+    else:
+        print(f"{name} Junior")
