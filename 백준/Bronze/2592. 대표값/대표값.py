707615ab537886248bee88279bc58e81ac464994
서성조 <67176549+seongjo-seo@users.noreply.github.com>
2023-01-27 23:58:02 +0900

    [Bronze II] Title: 대표값, Time: 60 ms, Memory: 34068 KB -BaekjoonHub

---
+++ 백준/Bronze/2592. 대표값/대표값.py
@@ -0,0 +1,14 @@
+import sys
+from collections import Counter
+
+total = 0
+result = [0] * 1001
+
+for i in range(10):
+    N = int( sys.stdin.readline() )
+    total += N
+    result[N] += 1
+
+
+print(int(total//10))
+print(result.index(max(result)))
