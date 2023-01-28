0c94e1ec2fe0e7e281bfbd3f6e8a8ddd3fb869c4
서성조 <67176549+seongjo-seo@users.noreply.github.com>
2023-01-29 02:14:26 +0900

    [Silver V] Title: 스네이크버드, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.py
@@ -0,0 +1,14 @@
+import sys
+
+N, Len_N = map( int, sys.stdin.readline().split() )
+
+req = list( map (int, sys.stdin.readline().split() ) )
+req.sort()
+
+for i in range(N):
+    if Len_N >= req[i]:
+        Len_N += 1
+    else:
+        break
+
+print(Len_N)
