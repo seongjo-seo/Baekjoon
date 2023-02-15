787ca35ae5dcb966d355af54df2d489997b9b719
서성조 <67176549+seongjo-seo@users.noreply.github.com>
2023-02-15 23:13:50 +0900

    [Silver IV] Title: 베스트셀러, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.py
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -1,23 +1,11 @@
 import sys
-from collections import Counter
 
 N = int(sys.stdin.readline())
 
 result = []
-cnt = {}
 
 for _ in range(N):
     result.append(sys.stdin.readline().split())
 result.sort()
 
-for i in result:
-    try:
-        cnt += i
-    except:
-        cnt = i
-
-res = Counter(cnt)
-
-data = res.most_common(n=1)
-
-print(data[0][0])
+print(*max(result, key=result.count))
