c0ad58cdbb1b15fed71885752cc7c2a1470a693e
서성조 <67176549+seongjo-seo@users.noreply.github.com>
2023-03-08 00:35:44 +0900

    [Silver V] Title: 덩치, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,17 +1,18 @@
 N = int(input())
 
-people = []
-rank = []
+body_list = []
 
 for _ in range(N):
-    x, y = map(int, input().split())
-    people.append((x, y))
+    weight, height = map(int, input().split())
+    body_list.append((weight, height))
 
-for person1 in people:
-    count = 1
-    for person2 in people:
-        if person1[0] < person2[0] and person1[1] < person2[1]:
-            count += 1
-    rank.append(count)
+for weight_x, height_y in enumerate(body_list):
+    rank = 1
 
-print(*rank)
+    for weight_i, height_j in enumerate(body_list):
+        if weight_x == weight_i:
+            continue
+        if height_j[0] > height_y[0] and height_j[1] > height_y[1]:
+            rank += 1
+
+    print(rank, end=' ')
