d145963f252eda55af2840f3b8a5387a121267ac
서성조 <67176549+seongjo-seo@users.noreply.github.com>
2023-03-10 23:52:47 +0900

    [Silver I] Title: 카드 구매하기, Time: 236 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,10 @@
+N = int(input())
+card_prices = list(map(int, input().split()))
+
+dp = [0] * (N+1)
+
+for i in range(1, N+1):
+    for j in range(0, i):
+        dp[i] = max(dp[i], dp[j] + card_prices[i-j-1])
+
+print(dp[N])
