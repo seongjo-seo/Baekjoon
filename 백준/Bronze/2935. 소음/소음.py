049c75f2c9a17f28ab00846c728a0e765b59acb1
서성조 <67176549+seongjo-seo@users.noreply.github.com>
2023-04-08 23:35:29 +0900

    [Bronze III] Title: 소음, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2935. 소음/소음.py
@@ -0,0 +1,13 @@
+A = int(input())
+op = input()
+B = int(input())
+
+
+if op == "+":
+    print(A+B)
+elif op == "-":
+    print(A-B)
+elif op == "*":
+    print(A*B)
+elif op == "%":
+    print(A%B)
