b8503195799f43dd9afc3522a2db0ae1b61ff6f0
서성조 <67176549+seongjo-seo@users.noreply.github.com>
2023-05-22 23:24:57 +0900

    [level 0] Title: 두 수의 차, Time: 0.02 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120803. 두 수의 차/두 수의 차.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        
+        if(answer<0){
+            answer = -1;   
+        }else{
+            answer = num1 - num2;
+        }
+        
+        return answer;
+    }
+}
