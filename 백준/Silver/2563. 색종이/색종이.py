89e6c0a2a7b832a624bbd653206ed2be3292afad
서성조 <67176549+seongjo-seo@users.noreply.github.com>
2023-06-10 23:48:48 +0900

    [Silver V] Title: 색종이, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,16 @@
+canvas = [[0] * 100 for _ in range(100)]
+
+n = int(input())
+for _ in range(n):
+    x, y = map(int, input().split())
+    for i in range(x, x+10):
+        for j in range(y, y+10):
+            canvas[i][j] = 1
+
+area = 0
+
+for i in range(100):
+    for j in range(100):
+        area += canvas[i][j]
+
+print(area)
