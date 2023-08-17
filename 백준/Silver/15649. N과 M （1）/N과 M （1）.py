019bf077a0f1a937d9792ac4eeb5e2366c6f8d34
서성조 <67176549+seongjo-seo@users.noreply.github.com>
2023-08-17 23:37:41 +0900

    [Silver III] Title: N과 M (1), Time: 264 ms, Memory: 38832 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,23 @@
+from typing import List
+
+def print_combinations(sequence: List[int], N: int, M: int) -> None:
+    # 종료 조건: 수열의 길이가 M과 같으면 출력
+    if len(sequence) == M:
+        print(" ".join(map(str, sequence)))
+        return
+
+    for i in range(1, N + 1):
+        if i not in sequence:  # 원소가 이미 선택된 경우는 제외
+            # 원소 선택하기
+            sequence.append(i)
+
+            # 다음 원소 선택을 위한 재귀 호출
+            print_combinations(sequence, N, M)
+
+            # 원소를 선택하지 않고 넘어가기
+            sequence.pop()
+
+N, M = map(int,input().split())
+
+print_combinations([], N, M)
+
