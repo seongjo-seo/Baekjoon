c32f6213cef8be14ec158b0f02eeb4d7e3f380da
서성조 <67176549+seongjo-seo@users.noreply.github.com>
2023-09-09 23:08:17 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,12 @@
+def solution(rsp):
+    answer = ''
+    
+    for char in rsp:
+        if char == "2":
+            answer += '0'
+        elif char == "0":
+            answer += '5'
+        elif char == "5":
+            answer += '2'
+    
+    return answer
