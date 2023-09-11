3c44368f25ac9528b198a981895bab0e635ee233
서성조 <67176549+seongjo-seo@users.noreply.github.com>
2023-09-11 22:44:16 +0900

    [unrated] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.py
@@ -1,13 +1,9 @@
 def solution(num_list):
     answer = 0
-    for idx, items in enumerate(num_list):
-        while(items > 1):
-            if items % 2 == 0:
-                items //= 2
-                answer +=1
-            elif items % 2 == 1:
-                items //= 2
-                answer +=1
-            
-            
+    for num in num_list:
+        while num > 1:
+            if num % 2:
+                num -= 1
+            num //= 2
+            answer += 1
     return answer
